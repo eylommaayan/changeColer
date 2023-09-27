f14405b8dc6565f4310e053e653c15898e8c3472
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2023-09-27 15:51:38 +0300

    Add files via upload
    changecolor  לחיצה על כפתור בשביל לשנות צבע
    	2- הוספת evets ו-function
    
    בפרויקט זה נשתמש בevents, functions

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import RandomizeColor from "./components/RandomizeColor";
+
+function App() {
+  return (
+    <>
+      <RandomizeColor />
+    </>
+  );
+}
+
+export default App;
